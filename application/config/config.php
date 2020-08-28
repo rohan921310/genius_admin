395bf35103fb878f5d39858c8db12d4cd5b32396
Rohan Sharma <rohan921310@gmail.com>
2020-08-29 00:13:02 +0530

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://SclMng.epizy.com/genius_admin/';
+$config['base_url'] = 'https://rohan921310.github.io/genius_admin/';
 
 /*
 |--------------------------------------------------------------------------
